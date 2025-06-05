ca2ab51f284a9c129a9638bee47c8543a69fe124
rickzor <paintedmist@gmail.com>
2025-06-04 21:36:09 -0700

    configure search bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {StyleSheet, Button, View, Text, Alert} from 'react-native';
 import ReactDOM from 'react-dom/client';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
@@ -8,7 +9,22 @@ import jsonData from './filmstocks.json';
 import reportWebVitals from './reportWebVitals';
 import logo from './img/desktop-logo-small.png'
 
+
+
 function Start() {
+  const [selectedValue, setSelectedValue] = useState(null);
+  const handleChange = (selectedValue) => {
+    setSelectedValue(selectedValue);
+  };
+  const handleGoClick = () => {
+        console.log('Selected Value:', selectedValue.value);
+        // Perform your desired action here
+    };
+  const styles = {
+  container: (base) => ({
+    ...base,
+    flex: 1,
+  })};
   return (
     <div >
       <div style={{
@@ -20,18 +36,26 @@ function Start() {
       </div>
 
       <div style={{
+      maxWidth: '600px',
+      display: 'flex',
       alignItems: 'center',
-      width: '500px',
       marginTop: '10px',
-      margin: 'auto',
+      margin: '0 auto',
       justifyContent: 'center'}}>
 
       <Select 
-        placeholder="Select a film stock"
+        styles={styles} 
+        placeholder="Select a film stock..."
+        onChange={handleChange}
+        value={selectedValue}
         options={jsonData.map((option) => ({
-        value: option.label,
-        label: option.film}))}/>
-      
+        value: option.process,
+        label: option.film}))
+        }/>
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <Button title='Start' onPress={handleGoClick}/>
       </div>
     </div>
   );
